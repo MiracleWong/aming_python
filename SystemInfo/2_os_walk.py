33cd27296f8dabfad71531c4a39a3d10f9ddd4b2
MiracleWong <cfwr1991@126.com>
2016-11-24 17:01:14 +0800

    add the os.walk

--- SystemInfo/2_os_walk.py
+++ SystemInfo/2_os_walk.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 import hashlib
-import sys
+import sys, os
 
 def md5sum(f):
 	m = hashlib.md5()
@@ -14,11 +14,15 @@ def md5sum(f):
 				break
 	return m.hexdigest()
 
-if __name__ == '__main__':
-	try:
-		print md5sum(sys.argv[1])
-	except IndexError:
-		print "%s follow a arguement: " % __file__
+s = ''
+a = os.walk(sys.argv[1])
+for p,d,f in a:
+	for i in f:
+		fn = os.path.join(p,i)
+		md5 = md5sum(fn)
+		s += md5 + ' ' + fn + '\n'
+
+print s,
 	
 
 
