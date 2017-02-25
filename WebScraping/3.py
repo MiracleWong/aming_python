ae93f6b3b3bf86f6b39630733cc2fa1c2cc66b7a
MiracleWong <cfwr1991@126.com>
2017-02-25 23:11:47 +0800

    get the correct message by re

--- WebScraping/3.py
+++ WebScraping/3.py
@@ -6,15 +6,39 @@ import urllib, urllib2
 import re
 
 
-page = 1
-url = 'http://www.qiushibaike.com/8hr/page/' + str(page);
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-try:
-	request = urllib2.Request(url,headers = headers)
-	response = urllib2.urlopen(request)
-	print response.read()
-except urllib2.URLError, e:
-	if hasattr(e,"code"):
-		print e.code
-	if hasattr(e, "reason"):
-		print e.code
+def getPage(page_num=1):
+	url = 'http://www.qiushibaike.com/8hr/page/' + str(page_num);
+	headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+	try:
+		request = urllib2.Request(url,headers = headers)
+		response = urllib2.urlopen(request)
+		html = response.read()
+		return html
+	except urllib2.URLError, e:
+		if hasattr(e,"code"):
+			print "连接服务器失败，错误代码：%s" %e.code
+			return None
+		if hasattr(e, "reason"):
+			print "连接服务器失败，错误原因：%s" %e.reason
+			return None
+
+def getPageContent(page_num=1):
+	html = getPage(page_num)
+	re_page = re.compile(r'<div class="author.*?>.*?<a.*?<img.*?alt="(.*?)"/>.*?<div class="content">(.*?)</div>.*?<div class="stats">.*?<i class="number">(\d+)</i>', re.S)
+
+	items = re_page.findall(html)
+	page_contents = []
+	replaceBR = re.compile(r'<br/>')
+
+	for item in items:
+		content = replaceBR.sub('\n', item[1])
+		page_contents.append([page_num, item[0].strip(), content.strip(), item[2].strip()])
+
+	return page_contents
+
+
+if __name__ == '__main__':
+	page_contents = getPageContent()
+	for item in page_contents:
+		for i in item:
+			print i
---
+++ WebScraping/4.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+
+import urllib, urllib2
+import re
+
+
+# page = 2
+# url = 'http://www.qiushibaike.com/8hr/page/' + str(page);
+url = "http://mp.weixin.qq.com/s/OrrT2RZ4J50Z22n1QYYyJw";
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+try:
+	request = urllib2.Request(url,headers = headers)
+	response = urllib2.urlopen(request)
+	html = response.read()
+	print html
+except urllib2.URLError, e:
+	if hasattr(e,"code"):
+		print "连接服务器失败，错误代码：%s" %e.code
+	if hasattr(e, "reason"):
+		print "连接服务器失败，错误原因：%s" %e.reason
+#
+# re_page = re.compile(r'<div class="author.*?>.*?<a.*?<img.*?alt="(.*?)"/>.*?<div class="content">(.*?)</div>.*?<div class="stats">.*?<i class="number">(\d+)</i>', re.S)
+#
+# items = re_page.findall(html)
+# for item in items:
+# 	for i in item:
+# 		print i
