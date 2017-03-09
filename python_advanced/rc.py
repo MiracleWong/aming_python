871ba0e87ba28090eae9640dc2843e9bffa00fd0
MiracleWong <cfwr1991@126.com>
2017-03-09 09:49:13 +0800

    add files

--- python_advanced/rc.py
+++ python_advanced/rc.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 
-class Peocess(object):
+from subprocess import Popen, PIPE
+import os
+
+class Process(object):
 	name = 'Test'
 
 	def __init__(self,name, program, args, workdir):
@@ -10,18 +13,54 @@ class Peocess(object):
 		self.args = args
 		self.workdir = workdir
 
+	def _init(self):
+		if not os.path.exitsts(self.workdir):
+			os.mkdir(self.workdir)
+			os.chdir(self.workdir)
+
+	def _pidFile(self):
+		return os.path.join(self.workdir, "%s.pid" % self.name)
+
 	def start(self):
-		
+		cmd = self.program + '' + self.args
+		p = Popen(cmd, stdout=PIPE, shell=True)
+
 	def stop(self):
-		
+
 
 	def restart(self):
 		self.stop()
 		self.start()
 
 	def status(self):
-		
-def amin():
+
+	def help(self):
+		# print ""
+
+def main():
+	name = 'memcached'
+	proc = '/usr/bin/memcached'
+	args = '-u nobody -p 11211 -c 1024 -m 64'
+	wd = '/var/tmp/memcached'
+	pm = Process(name = name, program = proc, args = args, workdir = wd)
 	try:
-		cmd = sys.argv[]
+		cmd = sys.argv[1]
 	except IndexError, e:
+		print "Option Error"
+		sys.exit()
+
+	if cmd == 'start':
+		pm.start()
+	elif cmd == 'stop':
+		pm.stop()
+	elif cmd == 'restart':
+		pm.restart()
+	elif cmd == 'status':
+		pm.status()
+	else:
+		pm.help()
+
+
+
+if __name__ == '__main__':
+	main()
