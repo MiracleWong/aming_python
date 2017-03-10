de01d6cbc21b2460790ab138e4275a28c605fc5e
MiracleWong <cfwr1991@126.com>
2017-03-10 19:46:52 +0800

    delete re,pyc

--- python_advanced/rc.py
+++ python_advanced/rc.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8 -*-
 
 class Process(object):
+<<<<<<< Updated upstream
     '''memcached rc script'''
     def __init__(self, name, program, args,workdir):
         self.name = name
@@ -59,3 +60,54 @@ def main():
 
 if __name__ == '__main__':
     main()
+=======
+	name = 'Test'
+
+	def __init__(self,name, program, args, workdir):
+		self.name = name
+		self.program = program
+		self.args = args
+		self.workdir = workdir
+
+	def start(self):
+
+	def stop(self):
+
+
+	def restart(self):
+		self.stop()
+		self.start()
+
+	def status(self):
+
+	def help(self):
+		# print ""
+
+def main():
+	name = 'memcached'
+	proc = '/usr/bin/memcached'
+	args = '-u nobody -p 11211 -c 1024 -m 64'
+	wd = '/var/tmp/memcached'
+	pm = Process(name = name, program = proc, args = args, workdir = wd)
+	try:
+		cmd = sys.argv[1]
+	except IndexError, e:
+		print "Option Error"
+		sys.exit()
+
+	if cmd == 'start':
+		pm.start()
+	elif cmd == 'stop':
+		pm.stop()
+	elif cmd == 'restart':
+		pm.restart()
+	elif cmd == 'status':
+		pm.status()
+	else:
+		pm.help()
+
+
+
+if __name__ == '__main__':
+	main()
+>>>>>>> Stashed changes
