3fc8501f3368cba8e22d0d431111a95708196b09
MiracleWong <cfwr1991@126.com>
2017-03-11 12:06:35 +0800

    perfect file

--- system_info/13_ip.py
+++ system_info/13_ip.py
@@ -10,7 +10,7 @@ def getIfconfig():
     return [i for i in data if i and not i.startswith('lo')]
 
 def paresIfconfig(data):
-    re_devname = re.compile(r'(br|eth|em|virbr|lo|bond)[\d:]+', re.M)
+    re_devname = re.compile(r'(br|eth|em|virbr|lo|bond)[\d:]', re.M)
     re_mac = re.compile(r'HWaddr ([0-9A-F:]{17})',re.M)
     re_ip = re.compile(r'inet addr:([\d\.]{7,15})',re.M)
     devname = re_devname.search(data)
@@ -31,5 +31,8 @@ def paresIfconfig(data):
     return {devname: [ip, mac]}
 if __name__ == '__main__':
     data =  getIfconfig()
+    dic_ip={}
     for i in data:
-        print paresIfconfig(data)
+        dic_ip.update(paresIfconfig(data))
+    for k,v in dic_ip.items():
+        print "%-5s: %-15s %s" %(k,v[0],v[1])
