0ead57181c73ea8bd34b42b5ac32d9376861247d
MiracleWong <cfwr1991@126.com>
2017-03-11 15:16:01 +0800

    perfect the 13_ip.py use regex

--- system_info/13_ip.py
+++ system_info/13_ip.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 import string
+import re
 from subprocess import Popen, PIPE
 
 def getIfconfig():
@@ -9,12 +10,24 @@ def getIfconfig():
     return [i for i in data if i and not i.startswith('lo')]
 
 def paresIfconfig(data):
-    re_devname = rc.compile(r'(br|eth|em|virbr|lo|bond)[\d:]+', re.M)
+    re_devname = re.compile(r'(br|eth|em|virbr|lo|bond)[\d:]+', re.M)
     re_mac = re.compile(r'HWaddr ([0-9A-F:]{17})',re.M)
     re_ip = re.compile(r'inet addr:()[\d\.]{7,15})',re.M)
-    devname = re_devname.rearsch(data).group()
-    mac = re_mac.rearsch(data).group(1)
+    devname = re_devname.search(data)
+    if devname:
+        devname = devname.group()
+    else:
+        devname = ''
+    mac = re_mac.search(data)
+    if mac:
+        mac = mac.group(1)
+    else:
+        mac = ''
     ip = re_ip.findall(data)
+    if ip:
+        ip = ip.group(1)
+    else:
+        ip=''
     return {devname: [ip, mac]}
 if __name__ == '__main__':
     data =  getIfconfig()
