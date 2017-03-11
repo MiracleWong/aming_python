8c9fd02d47c48f9b310a8e3c818509f4e80f422c
MiracleWong <cfwr1991@126.com>
2017-03-11 15:00:24 +0800

    perfect the 13_ip.py use regex

--- system_info/13_ip.py
+++ system_info/13_ip.py
@@ -9,15 +9,14 @@ def getIfconfig():
     return [i for i in data if i and not i.startswith('lo')]
 
 def paresIfconfig(data):
-    dic = {}
-    for lines in data:
-        line_list = lines.split('\n')
-        devname = line_list[0].split()[0]
-        macaddr = line_list[0].split()[-1]
-        ipaddr = line_list[1].split()[1].split(':')[1]
-        dic[devname] = [ipaddr, macaddr]
-    return dic
-
+    re_devname = rc.compile(r'(br|eth|em|virbr|lo|bond)[\d:]+', re.M)
+    re_mac = re.compile(r'HWaddr ([0-9A-F:]{17})',re.M)
+    re_ip = re.compile(r'inet addr:()[\d\.]{7,15})',re.M)
+    devname = re_devname.rearsch(data).group()
+    mac = re_mac.rearsch(data).group(1)
+    ip = re_ip.findall(data)
+    return {devname: [ip, mac]}
 if __name__ == '__main__':
     data =  getIfconfig()
+    for i in data:
     print paresIfconfig(data)
