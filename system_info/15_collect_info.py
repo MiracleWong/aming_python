488d88744f9d407c5248512c1dde2240cffa45c8
MiracleWong <cfwr1991@126.com>
2017-03-15 09:09:53 +0800

    Add the data push for the Django website

--- system_info/15_collect_info.py
+++ system_info/15_collect_info.py
@@ -102,3 +102,6 @@ if __name__ == '__main__':
     dic.update(cpu)
     dic.update(mem)
     print dic
+    data = urllib.urlencode(dic)
+    req=urllib2.urlopen('http://127.0.0.1:8000/hostinfo/collect/',data)
+    print req.read()
