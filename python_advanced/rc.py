ed0c5f7e0449520564d06ff5730b3b846d98b909
MiracleWong <cfwr1991@126.com>
2017-03-20 16:31:15 +0800

    class definition and the structure of the script

--- python_advanced/rc.py
+++ python_advanced/rc.py
@@ -22,10 +22,29 @@ class Process(object):
         self.start()
     def status(self):
         pass
-
+    def help(self):
+        pass 
 def main():
+    name = 'memcached' 
+    prog = '/usr/bin/memcached'
+    args = '-u nobody -p 11211 -c 1024 -m 64'
+    wd = '/var/tmp/memcached'
+    pm = Process(name = name, program = prog, args = args, workdir = wd)
     try:
         cmd = sys.argv[1]
     except IndexError, e:
         print "Option Error"
         sys.exit()
+    if cmd == 'start':
+        pm.start()
+    elif cmd == 'stop':
+        pm.stop()
+    elif cmd == 'restart':
+        pm.restart()
+    elif cmd == 'status':
+        pm.status()
+    else:
+        pm.help()
+
+if __name__ == '__main__'
+    main()
