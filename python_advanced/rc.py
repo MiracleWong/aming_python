38330f7883f388e62f24ed6d4ed79036aba81b9a
MiracleWong <cfwr1991@126.com>
2017-03-20 17:03:59 +0800

    comlete the start function

--- python_advanced/rc.py
+++ python_advanced/rc.py
@@ -1,20 +1,39 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 
-import sys
+import sys, os
+import subprocess import Popen, PIPE
 class Process(object):
     '''memcached rc script'''
     def __init__(self, name, program, args, workdir):
         self.name = name
         self.program = program
         self.args = args
-        sele.workdir = workdir
+        self.workdir = workdir
 
     # Normal Function
     def _init(self):
-        pass
+        '''/var/tmp/memcached'''
+        if not os.path.exists(self.workdir):
+        	os.mkdir(self.workdir)
+        	os.chdir(self.workdir)
+
+
+    def _pidFile(self):
+    	'''/var/tmp/memcached.pid'''
+    	return os.path.join(self.workdir, "%s.pid" % self.name)
+
+    def _writePid(self):
+    	if self.pid:
+    		with open(self._pidFile(), 'w') as fd:
+    			fd.write(str(self.pid))
+
     def start(self):
-        pass
+        cmd = self.program + ' ' + self.args
+        p = Popen(cmd, stdout = PIPE, shell = True)
+        self.pid = p.pid
+        self._writePid()
+        print "%s start Sucessful" % self.name
     def stop(self):
         pass
     def restart(self):
@@ -24,27 +43,32 @@ class Process(object):
         pass
     def help(self):
         pass 
-def main():
-    name = 'memcached' 
-    prog = '/usr/bin/memcached'
-    args = '-u nobody -p 11211 -c 1024 -m 64'
-    wd = '/var/tmp/memcached'
-    pm = Process(name = name, program = prog, args = args, workdir = wd)
-    try:
-        cmd = sys.argv[1]
-    except IndexError, e:
-        print "Option Error"
-        sys.exit()
-    if cmd == 'start':
-        pm.start()
-    elif cmd == 'stop':
-        pm.stop()
-    elif cmd == 'restart':
-        pm.restart()
-    elif cmd == 'status':
-        pm.status()
-    else:
-        pm.help()
 
-if __name__ == '__main__'
-    main()
+def main():
+	name = 'memcached'
+	proc = '/usr/bin/memcached'
+	args = '-u nobody -p 11211 -c 1024 -m 64'
+	wd = '/var/tmp/memcached'
+	pm = Process(name = name, program = proc, args = args, workdir = wd)
+	try:
+		cmd = sys.argv[1]
+	except IndexError, e:
+		print "Option Error"
+		sys.exit()
+
+	if cmd == 'start':
+		pm.start()
+	elif cmd == 'stop':
+		pm.stop()
+	elif cmd == 'restart':
+		pm.restart()
+	elif cmd == 'status':
+		pm.status()
+	else:
+		pm.help()
+
+
+
+if __name__ == '__main__':
+	main()
+
