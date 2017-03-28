3ec8c6ab18e17ea95a34118139ba7dd4b70e9f85
MiracleWong <cfwr1991@126.com>
2017-03-28 10:51:52 +0800

    status check by fabirc

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,18 @@
 #-*- coding:utf-8 -*-
 
 from fabric.api import run
+from fabric.api import env
+
+env.user = 'test'
+env.password = 'test'
+
+env.hosts = ['localhost', '192.168.1.6']
 
 def host_type():
-    run('uname -s')
+    check_ver()
+
+def check_ver():
+	run("""LIVE_VER=`curl -s http://192.168.72.136:8080/deploy/livever`
+    	LIVE_WP=/var/www/deploy/wordpress-$LIVE_VER
+    	test -d $LIVE_WP && echo "$LIVE_WP is exists"
+    	""")
